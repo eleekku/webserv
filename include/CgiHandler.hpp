ba19e3849db5a3b0d6508c1d65074e7979b074fd
Deivy Zurita <deivylex93@gmail.com>
2025-01-13 14:00:32 +0200

    cgi execute ready for test

--- include/CgiHandler.hpp
+++ include/CgiHandler.hpp
@@ -1,12 +1,16 @@
 #pragma once
 
 #include <string>
+#include <unistd.h>
+#include <iostream>
+#include <sys/wait.h>
 
 class CgiHandler
 {
 public:
+
 	CgiHandler();
 	~CgiHandler();
 
-	std::string executeCgi(const std::string& path);
+	std::string executeCGI(std::string scriptPath, std::string queryString, std::string body, int method);
 };
